8b7d85870f9e72c9b86cfcd2418423f03193ebdd
ILIA <dubenskiiilia@gmail.com>
2023-05-17 14:58:28 +0300

    Initial Commit

---
+++ TradingViewApp/TradingViewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingViewAppApp.swift
+//  TradingViewApp
+//
+//  Created by Илья Дубенский on 17.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct TradingViewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
